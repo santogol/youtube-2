36495e232c3d3f505d060e14d2f9a23718c01faa
santogol <riccardosantomauro58@gmail.com>
2025-06-11 15:45:25 +0200

    Create PostSchema.js

---
+++ public/model/PostSchema.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Utente",
+    required: true
+  },
+  text: {
+    type: String,
+    required: true,
+    maxlength: 200
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Utente",
+      required: true
+    },
+    desc: {
+      type: String,
+      maxlength: 500,
+      default: ""
+    },
+    Image: {
+      type: String
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Utente"
+      }
+    ],
+    comments: [CommentSchema]
+  },
+  { timestamps: true }
+);
+
+// Rimuove automaticamente i post 24h dopo la creazione
+PostSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 });
+
+module.exports = mongoose.model("Post", PostSchema);
